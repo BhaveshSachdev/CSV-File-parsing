86a965f7d23ea18ddd776a2cbd461a345a267200
Bhavesh Sachdev <sachdev.b@husky.neu.edu>
2017-12-07 23:36:41 -0500

    Creating application structure

---
+++ README.txt
@@ -0,0 +1,6 @@
+Pre-requisites for running this
+ application:
+--------------------------------------------------------------------
+
+- Java 8
+- NetBeans or any other Java based IDE
---
+++ src/MyPackage/CSVData.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyPackage;
+
+/**
+ *
+ * @author Bhavesh
+ */
+public class CSVData 
+{
+    private String firstName, lastName, State;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
